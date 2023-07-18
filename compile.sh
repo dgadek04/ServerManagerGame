a9d6fa9e6574b1ec19d89fe00d25b195bb34d3a2
dawid <dawidgadek04@gmail.com>
2023-07-18 19:38:44 +0100

    Added a compile script.

---
+++ compile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set the output file name
+output_file="game"
+
+# List all the source files to be compiled
+source_files=(
+    "src/main.cpp"
+    "src/game.cpp"
+    # Add more source files as needed
+)
+
+# Compile the source files
+g++ -o "$output_file" "${source_files[@]}"
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Compilation successful!"
+else
+    echo "Compilation failed."
+fi
