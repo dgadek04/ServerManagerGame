92314b2fb7b4f649510ec4cafb9e7aca5b11b03d
dawid <dawidgadek04@gmail.com>
2023-07-19 01:06:23 +0100

    Add server-related features to Player class
    - Add 'servers_owned' member variable to track the number of servers owned by the player.\n- Add 'server_list' member variable as a vector of strings to store the list of servers.

--- src/player.h
+++ src/player.h
@@ -1,12 +1,14 @@
 #ifndef PLAYER_H
 #define PLAYER_H
-
+#include <vector>
+#include <string>
 class Player{
     
     private:
     int balance;
-
     public:
+    int servers_owned = 0;
+    std::vector<std::string> server_list;
     int get_balance();
     void set_balance(int new_balance);
     
