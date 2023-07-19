3e795afb03a038d9b177aec666fd4f64b4d1c085
dawid <dawidgadek04@gmail.com>
2023-07-19 01:12:11 +0100

    Refactor game.cpp file
    Refactor the game.cpp file by making the following changes:
    
    - Removed unnecessary includes.
    - Added a clear screen function (cls) to clear the console.
    - Created a Server class with name, ownership status, cost, and RAM attributes.
    - Implemented an income function to simulate income generation for the player.
    - Initialized the first server (server1) with a name, cost, and RAM.
    - Initialized the player with a starting balance of 400.
    - Added a game loop that displays the player's balance and number of servers owned.
    - Implemented player input for buying and selling servers.
    - Improved error handling for incorrect arguments.
    - Ensured proper synchronization and ownership updates for the server and player.
    - Implemented a start function to start the game and an accompanying stop function to end the game.
    
    These changes enhance the functionality and readability of the game.cpp file.

--- src/game.cpp
+++ src/game.cpp
@@ -1,24 +1,80 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 #include "game.h"
 #include "player.h"
 
-void cls(){
+void cls() {
     system("clear");
 }
-std::string player_input; 
 
-void Game::start(){
+class Server {
+public:
+    std::string name;
+    bool is_owned = false;
+    int cost;
+    int ram;
+};
+
+void income(Player& player, Server& server, int worth, int delay_seconds) {
+    while(true){
+        while (server.is_owned) {
+            std::this_thread::sleep_for(std::chrono::seconds(delay_seconds));
+            player.set_balance(player.get_balance() + worth);
+            }
+        }
+    }
+
+std::string player_input;
+
+void Game::start() {
+    // Initialize first server.
+    Server server1;
+    server1.name = "testserver";
+    server1.cost = 400;
+    server1.ram = 4 * 1024;
+
     Player player;
+    player.set_balance(400);
+
     game_status = true;
 
-    while(game_status == true){
+    std::thread incomeThread(income, std::ref(player), std::ref(server1), 10, 10); // Start the income generation thread // This takes in the function 'income' (&Player, worth, delay in seconds)
+
+    while (game_status) {
         cls();
-        std::cout<<"Balance:"<<" "<<player.get_balance()<<std::endl;
-        std::cout<<"<Player>:"<<" ";std::cin>>player_input;
+        std::cout << "Balance: " << player.get_balance() << std::endl;
+        std::cout << "Servers: " << player.servers_owned << std::endl;
+        std::cout << "<Player>: ";
 
+        std::cin >> player_input;
 
+        if (player_input == "buy") {
+            if (!server1.is_owned) {
+                player.servers_owned = 1;
+                player.server_list.push_back(server1.name);
+                player.set_balance(player.get_balance() - server1.cost);
+                server1.is_owned = true; // Update the ownership flag
+            }
+        }
+        else if (player_input == "sell") {
+            if (server1.is_owned) {
+                player.servers_owned = 0;
+                player.server_list.clear();
+                player.set_balance(player.get_balance() + server1.cost);
+                server1.is_owned = false; // Update the ownership flag
+            }
+        }
+        else {
+            cls();
+            std::cout << "Incorrect argument" << std::endl;
+        }
     }
+
+    server1.is_owned = false; // Stop the income generation thread before exiting
+    incomeThread.join();
 }
-void Game::stop(){
+
+void Game::stop() {
     game_status = false;
 }
